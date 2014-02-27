7aea4582c08cf8fec7496e198de18ce539942518
Joshua Petitt <joshpetitt@jpmec.com>
2014-02-26 21:19:18 -0600

    Changed scripts to non-minified version for debugging.

--- web/src/app/index_scripts.php
+++ web/src/app/index_scripts.php
@@ -1,12 +1,12 @@
 <!-- build:js js/app.js -->
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/angular.min.js"></script>
-<script type="text/javascript" src="js/angular-route.min.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/angular.js"></script>
+<script type="text/javascript" src="js/angular-route.js"></script>
 <script type="text/javascript" src="js/lodash.min.js"></script>
 <script type="text/javascript" src="js/moment.min.js"></script>
 <script type="text/javascript" src="js/ng-grid-2.0.7.min.js"></script>
-<script type="text/javascript" src="js/ui-bootstrap-tpls.min.js"></script>
+<script type="text/javascript" src="js/ui-bootstrap-tpls.js"></script>
 
 <script type="text/javascript" src="js/Chart.min.js"></script>
 
